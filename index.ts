659878ba6f6287a6f53be151cf08e085691898e8
Hung Bui <hungbui7690@gmail.com>
2023-03-10 08:50:53 -0700

    When Inference Doesn't Work P2

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 /*
-  When Inference Doesn't Work P1
+  When Inference Doesn't Work P2
 
 */
 
 // 3) Variable whose type cannot be inferred correctly
 let numbers = [-10, -1, 12]
-let numberAboveZero = false // this variable in this case maybe users, blog posts whereas there's no way for us to predict the values
+
+// (***) fix > either boolean or number
+let numberAboveZero: boolean | number = false
 
 for (let i = 0; i < numbers.length; i++) {
   if (numbers[i] > 0) {
