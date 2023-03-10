449b555de315e7e41e8d82858f5c621da057ad13
Hung Bui <hungbui7690@gmail.com>
2023-03-10 13:02:12 -0700

    Classes

--- index.ts
+++ index.ts
@@ -1,37 +1,18 @@
 /*
-  General Plan with Interfaces
+  Classes
   > pic
   
 */
 
-interface Reportable {
-  summary(): string
+class Vehicle {
+  drive(): void {
+    console.log(`bruhnnnnnnn!!!`)
+  }
+  honk(): void {
+    console.log('beep!!')
+  }
 }
 
-const printItem = (item: Reportable): void => {
-  console.log(item.summary())
-}
-
-const oldCivic = {
-  name: 'civic',
-  year: new Date(),
-  broken: true,
-  summary(): string {
-    return `Name: ${this.name}`
-  },
-}
-
-// (***) both oldCivic & drink has summary() > consider as Reportable type
-const drink = {
-  color: 'brown',
-  carbonated: true,
-  sugar: 40,
-  summary(): string {
-    return `My drink has ${this.sugar} grams of sugar`
-  },
-}
-
-printItem(oldCivic)
-
-// (***)
-printItem(drink)
+const vehicle = new Vehicle()
+vehicle.drive()
+vehicle.honk()
