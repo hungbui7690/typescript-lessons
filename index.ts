1417e6352a8c56335e4dce7e00cc74e049724953
Hung Bui <hungbui7690@gmail.com>
2023-03-10 09:06:46 -0700

    More on Annotations Around Functions P1

--- index.ts
+++ index.ts
@@ -1,16 +1,12 @@
 /*
-  When Inference Doesn't Work P2
-
+  More on Annotations Around Functions P1
+  - pic: func
 */
 
-// 3) Variable whose type cannot be inferred correctly
-let numbers = [-10, -1, 12]
-
-// (***) fix > either boolean or number
-let numberAboveZero: boolean | number = false
-
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > 0) {
-    numberAboveZero = numbers[i]
-  }
+// previous > nasty
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
 }
+
+// (***) work here
+const add = (a, b) => {}
