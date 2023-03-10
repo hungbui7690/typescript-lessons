9df51b5672ebb0098489bc3b25a77ab46b2df498
Hung Bui <hungbui7690@gmail.com>
2023-03-10 09:07:12 -0700

    More on Annotations Around Functions P2

--- index.ts
+++ index.ts
@@ -1,12 +1,7 @@
 /*
-  More on Annotations Around Functions P1
+  More on Annotations Around Functions P2
   - pic: func
 */
 
-// previous > nasty
-const logNumber: (i: number) => void = (i: number) => {
-  console.log(i)
-}
-
-// (***) work here
-const add = (a, b) => {}
+// (***) hover
+const add = (a: number, b: number): number => {}
