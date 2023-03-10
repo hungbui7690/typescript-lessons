eed7b7936df94eaf04892cf33f6f1e0c481e77cd
Hung Bui <hungbui7690@gmail.com>
2023-03-10 05:11:57 -0700

    TS Setup & A First App

---
+++ index.ts
@@ -0,0 +1,26 @@
+/*
+  Typescript Overview
+  - Javascript + Type System
+    > pic: ts-overview
+
+  > npm install -g typescript
+  > npm install ts-node --save-dev
+    > one time setup
+
+  > tsc --help
+
+///////////////////////////////////////
+
+  A First App
+  
+  - make a network request to fetch some JSON and print the result 
+    > https://jsonplaceholder.typicode.com/todos
+    > https://jsonplaceholder.typicode.com/todos/1
+    
+  - pic > first-app
+  
+  (1) npm init -y
+    > create package.json
+  (2) npm install axios
+  
+*/
