4fdaf9bb2777d7d223fa329967af86262abaa8dc
Hung Bui <hungbui7690@gmail.com>
2023-03-10 14:41:24 -0700

    App Overview & Bundling with Parcel

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/*
+  App Overview
+  
+  - Parcel-Bundler > tool to help us run TS in the browser
+    > npm install --save-dev parcel
+    
+
+  (***) we will learn how to setup TS project manually later > now use parcel-bundler first
+
+////////////////////////////////////////
+
+  Bundling with Parcel
+
+  (1) create ./index.html
+  (2) create [src]/index.ts
+
+  to run: 
+  > npx parcel index.html
+
+*/
+
+console.log('hello')
