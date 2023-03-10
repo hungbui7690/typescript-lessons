3c9f01a3ac0901694410fa598e7f13158c835c35
Hung Bui <hungbui7690@gmail.com>
2023-03-10 11:37:03 -0700

    Tuples in Typescript

--- index.ts
+++ index.ts
@@ -1,15 +1,24 @@
 /*
-  Multiple Types in Arrays
-  
+  Tuples in Typescript
+  > pic: tuple
+
 */
 
-// Flexible types
-const importantDates = [new Date(), '2030-10-10'] // hover > string | Date
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+}
 
-const importantDatesX: (string | Date)[] = [new Date(), '2030-10-10']
-importantDatesX.push(new Date())
-importantDatesX.push('2033-01-01')
+// hover > we don't see the format > now, in this array, this can be string OR boolean OR number
+const pepsi = ['brown', true, 40]
+pepsi[0] = 40
+pepsi[2] = 'brown' // (***) if we do this, we list information
 
-// (***) if empty array > need to have type > otherwise, any
-const fruits = [] // any
-const numbers: number[] = [] // number
+// (***) tuple > have the order
+const coke: [string, boolean, number] = ['brown', true, 40]
+coke[0] = 40 // err
+
+// (***) tuple: alternative method > use TYPE ALIAS === type keyword
+type Drink = [string, boolean, number]
+const tea = ['brown', false, 0]
