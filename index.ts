aee31a8e3c042cd508eaca5c5085a17b74741d79
Hung Bui <hungbui7690@gmail.com>
2023-03-10 06:44:25 -0700

    Annotations Around Functions

--- index.ts
+++ index.ts
@@ -1,21 +1,17 @@
 /*
-  Object Literals Annotations
+  Annotations Around Functions
 
 */
 
-// Array
-let colors: string[] = ['red', 'green', 'blue']
-let numbers: number[] = [1, 2, 3]
-let truths: boolean[] = [true, false]
-
-// Classes
-class Car {}
-
-let carX: Car
-carX = new Car()
-
-// Object Literals
-let point: { x: number; y: number } = {
-  x: 10,
-  y: 20,
+const logNumber = (i: number) => {
+  console.log(i)
 }
+
+// hover
+const logNumberX: (i: number) => void = (i: number) => {
+  console.log(i)
+}
+
+let apples: number = 5
+let orange = 5 // hover > number > that's type inference
+let grapes
