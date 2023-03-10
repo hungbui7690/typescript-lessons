ad310f3e285cdcd89e212e0d000a2a638a76bc5c
Hung Bui <hungbui7690@gmail.com>
2023-03-10 08:44:33 -0700

    Delayed Initialization P1

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 /*
-  Fixing the Any Type
+  Delayed Initialization P1
   - pic: inference, any
 */
 
-const json = '{"x" : 10, "y": 20}'
+// 2) when we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue']
+let foundWord // hover > type === any
 
-// add type annotation to fix > hover on variable
-const coordinates: { x: number; y: number } = JSON.parse(json)
-
-console.log(coordinates)
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') foundWord = true
+}
