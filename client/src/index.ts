fcb1c5cb0dfc4d3aecff3cb798c9978d5c02de45
hungbui7690 <hungbui7690@gmail.com>
2023-03-24 03:34:58 +0700

    Two Huge Issues & Typescript is Really Smart

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,22 +1,39 @@
 /*
-  Sorting Implementation
+  Two Huge Issues
+  - now, this algo works with only number[]
+  - if, collection: string > won't work
+  - pic:
+    + string is immutable
+    + comparison does not work well
+
+//////////////////////////////////
+
+  Typescript is Really Smart
+  - pic
+
 
 */
 
 class Sorter {
-  constructor(public collection: number[]) {}
+  // *** collection: number[] | string
+  constructor(public collection: number[] | string) {}
 
-  // ***
   sort(): void {
-    const { length } = this.collection // number
+    const { length } = this.collection
+
+    // this.collection. // *** we can see the common methods & properties of number[] & string
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i; j++) {
+        // *** number[]
         if (this.collection[j] > this.collection[j + 1]) {
           const temp = this.collection[j]
-          this.collection[j] = this.collection[j + 1]
+          this.collection[j] = this.collection[j + 1] // err because string does not support update > hover to see the error
           this.collection[j + 1] = temp
         }
+
+        // *** string
+        // ~~~ logic to compare and swap characters in a string
       }
     }
   }
