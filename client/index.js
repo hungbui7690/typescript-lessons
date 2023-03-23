efdfd5df4074be16b5ce47c77a158e7a6acb661c
hungbui7690 <hungbui7690@gmail.com>
2023-03-24 01:58:52 +0700

    App Overview & Configuring the TS Compiler P1

---
+++ client/index.js
@@ -0,0 +1,12 @@
+/*
+  App Overview
+  - pic
+  - sorting logic > we want to have only 1 sorting algorithm for those 3 data structures
+
+////////////////////////////////////
+
+  Configuring the TS Compiler
+
+
+*/
+console.log('hello world');
