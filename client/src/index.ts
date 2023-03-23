a1c830b6be431b23e987804aa266a61814477860
hungbui7690 <hungbui7690@gmail.com>
2023-03-24 06:00:12 +0700

    App Overview & Project Setup

---
+++ client/src/index.ts
@@ -0,0 +1,18 @@
+/*
+  App Overview
+  - pic
+
+    > npm init -y
+    > tsc --init
+      + setup rootDir + outDir
+    > npm install nodemon concurrently 
+  
+  - package.json: 
+      "scripts": {
+          "start:build": "tsc --w",
+          "start:run": "nodemon dist/index.js",
+          "start": "concurrently npm:start:*"
+        },
+*/
+
+console.log('Hello World')
