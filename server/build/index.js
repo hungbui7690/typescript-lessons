44b323a390e5c509cbbaf0e57be5a0c2147107a4
hungbui7690 <hungbui7690@gmail.com>
2023-03-25 02:33:06 +0700

    Basic Routes with Express

--- server/build/index.js
+++ server/build/index.js
@@ -1,27 +1,27 @@
 "use strict";
 /*
-  Typescript with JS Libraries
-  - pic
-  - as we learn so far about TS, we use classes and interfaces to connect the classes together > but in the real world, we don't use classes too much
-    > https://www.npmjs.com/package/ts-express-decorators
-      > check this link, we can see most of the code using class, which we don't use too much in Express
-    >
-  - because we don't want to use classes too much, we will need a TS adapter library that has helpers for using lib with TS
-  - or Twist lib to work with TS classes (most of the time we will use this)
-
-////////////////////////////////////////
-
-  App Overview
-  - pic
-  - we will first work with Express normally, with basic Type Annotation > then later, we will twist it to work with TS classes
-
-//////////////////////////////////////////
-
-  Project Setup
-  - tsc --init
-  - npm install nodemon concurrently
-  - setup outDir & rootDir
-  - package.json
+  Basic Routes with Express
+  > npm install express body-parser cookie-session
+    + body-parser: parse body
+    + cookie-session: store login info
+  > npm install @types/express @types/cookie-session @types/body-parser
 
 */
-console.log('Hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// *** ctrl + click > search for "Request" > we will see interfaces Request
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// *** type annotation here: depends on how deep we want to integrate with TS
+app.get('/', (req, res) => {
+    res.send(`
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  `);
+});
+app.listen(5000, () => {
+    console.log(`Listening on port 5000...`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,25 @@
 /*
-  Typescript with JS Libraries
-  - pic
-  - as we learn so far about TS, we use classes and interfaces to connect the classes together > but in the real world, we don't use classes too much
-    > https://www.npmjs.com/package/ts-express-decorators
-      > check this link, we can see most of the code using class, which we don't use too much in Express
-    > 
-  - because we don't want to use classes too much, we will need a TS adapter library that has helpers for using lib with TS 
-  - or Twist lib to work with TS classes (most of the time we will use this)
-
-////////////////////////////////////////
-
-  App Overview
-  - pic
-  - we will first work with Express normally, with basic Type Annotation > then later, we will twist it to work with TS classes
-
-//////////////////////////////////////////
-
-  Project Setup
-  - tsc --init
-  - npm install nodemon concurrently
-  - setup outDir & rootDir
-  - package.json
+  Basic Routes with Express
+  > npm install express body-parser cookie-session
+    + body-parser: parse body 
+    + cookie-session: store login info
+  > npm install @types/express @types/cookie-session @types/body-parser
 
 */
 
-console.log('Hi there')
+// *** ctrl + click > search for "Request" > we will see interfaces Request
+import express, { Request, Response } from 'express'
+const app = express()
+
+// *** type annotation here: depends on how deep we want to integrate with TS
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  `)
+})
+
+app.listen(5000, () => {
+  console.log(`Listening on port 5000...`)
+})
