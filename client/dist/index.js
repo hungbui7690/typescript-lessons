1aab25b5103358e871da4e35cfa31b690bc3627a
hungbui7690 <hungbui7690@gmail.com>
2023-03-24 07:32:39 +0700

    Type Definition Files Again P2

--- client/dist/index.js
+++ client/dist/index.js
@@ -1,10 +1,8 @@
 "use strict";
 /*
-  Type Definition Files Again P1
-  - check csv file > each row represents a football match
-  - will use nodeJS standard library to load data
-    > https://nodejs.org/dist/latest-v18.x/docs/api/fs.html
-      > readFileSync()
+  Type Definition Files Again P2
+  - npm install @types/node
+
 
 */
 Object.defineProperty(exports, "__esModule", { value: true });
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,8 @@
 /*
-  Type Definition Files Again P1
-  - check csv file > each row represents a football match
-  - will use nodeJS standard library to load data
-    > https://nodejs.org/dist/latest-v18.x/docs/api/fs.html
-      > readFileSync()
+  Type Definition Files Again P2
+  - npm install @types/node
+
 
 */
 
-import { readFileSync } from 'fs' // error > this is because TS reports the wrong error for us > pic
+import { readFileSync } from 'fs'
