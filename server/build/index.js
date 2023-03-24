be1272c202cbcd2b5d32be84023ef0c5945ce588
hungbui7690 <hungbui7690@gmail.com>
2023-03-25 02:36:27 +0700

    Using an Express Router

--- server/build/index.js
+++ server/build/index.js
@@ -1,27 +1,17 @@
 "use strict";
 /*
-  Basic Routes with Express
-  > npm install express body-parser cookie-session
-    + body-parser: parse body
-    + cookie-session: store login info
-  > npm install @types/express @types/cookie-session @types/body-parser
+  Using an Express Router
+  - create routes/loginRoutes.ts
 
 */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// *** ctrl + click > search for "Request" > we will see interfaces Request
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-// *** type annotation here: depends on how deep we want to integrate with TS
-app.get('/', (req, res) => {
-    res.send(`
-    <div>
-      <h1>Hi there!</h1>
-    </div>
-  `);
-});
+const loginRoutes_1 = require("./routes/loginRoutes"); // ***
+app.use(loginRoutes_1.router);
 app.listen(5000, () => {
     console.log(`Listening on port 5000...`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,14 @@
 /*
-  Basic Routes with Express
-  > npm install express body-parser cookie-session
-    + body-parser: parse body 
-    + cookie-session: store login info
-  > npm install @types/express @types/cookie-session @types/body-parser
+  Using an Express Router
+  - create routes/loginRoutes.ts
 
 */
 
-// *** ctrl + click > search for "Request" > we will see interfaces Request
 import express, { Request, Response } from 'express'
 const app = express()
+import { router } from './routes/loginRoutes' // ***
 
-// *** type annotation here: depends on how deep we want to integrate with TS
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi there!</h1>
-    </div>
-  `)
-})
+app.use(router)
 
 app.listen(5000, () => {
   console.log(`Listening on port 5000...`)
