5612e27174250c486d9ab31f3116df6223ecd2af
hungbui7690 <hungbui7690@gmail.com>
2023-03-24 07:35:52 +0700

    Reading CSV Files P1

--- client/dist/index.js
+++ client/dist/index.js
@@ -1,8 +1,16 @@
 "use strict";
 /*
-  Type Definition Files Again P2
-  - npm install @types/node
-
+  Reading CSV Files P1
+  - return all matches, which is not good > we need to parse these data > pic > we want to convert to 2D array of strings
+    > next lessons
 
 */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8', // if we don't add this option > return buffer
+});
+console.log(matches);
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,8 +1,14 @@
 /*
-  Type Definition Files Again P2
-  - npm install @types/node
-
+  Reading CSV Files P1
+  - return all matches, which is not good > we need to parse these data > pic > we want to convert to 2D array of strings 
+    > next lessons
 
 */
 
-import { readFileSync } from 'fs'
+import fs from 'fs'
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8', // if we don't add this option > return buffer
+})
+
+console.log(matches)
