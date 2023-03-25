db7c4c55da4aaf8eb91e65062fe6fd76a6bf6eec
hungbui7690 <hungbui7690@gmail.com>
2023-03-25 10:31:18 +0700

    Details on Decorators

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,27 @@
 /*
-  Decorators in Typescript
-  - "target": "es5" 
-  - "experimentalDecorators": true
-  - "emitDecoratorMetadata": true
-
+  Details on Decorators
   - pic
+    > decorators will be applied single time when a class is create > not when instantiating an object
 
 */
 
 class Boat {
-  color: string = 'red'
+  color: string = 'red' // property
 
+  // accessor
   get formattedColor(): string {
     return `This boat color is ${this.color}`
   }
 
-  @testDecorator // apply decorator
+  // @testDecorator // method
   pilot(): void {
     console.log('swish')
   }
 }
 
-// *** define decorator
 function testDecorator(target: any, key: string): void {
-  console.log(target)
-  console.log(key)
+  console.log(target) // { pilot: [Function (anonymous)] } > prototype of class boat
+  console.log(key) // pilot
 }
+
+testDecorator(Boat.prototype, 'pilot') // *** remove @decorator above > this is the way we run > just syntactic sugar
