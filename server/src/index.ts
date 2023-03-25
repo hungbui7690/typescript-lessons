d8cb9f3921840fb509106672b436ac1d69a65b17
hungbui7690 <hungbui7690@gmail.com>
2023-03-25 12:03:27 +0700

    Decorators Around Properties

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,32 @@
 /*
-  Decorator Factories
+  Decorators Around Properties 
 
 */
 
 class Boat {
+  @testDecorator // ***
   color: string = 'red'
 
   get formattedColor(): string {
     return `This boat color is ${this.color}`
   }
 
-  @logError('Oops, boat was sunk in ocean') // *** pass parameter to decorator
+  @logError('Oops, boat was sunk in ocean')
   pilot(): void {
     throw new Error('')
   }
 }
 
-// *** decorator factory: decorator that returns function
+// ***
+function testDecorator(target: any, key: string) {
+  console.log(target)
+  console.log(key)
+
+  // print out the value
+  console.log(target.value) // wrong: because target points to prototype > always store methods > cannot access instance members > but decorator is executed b4 we create the instance > there's no way to access instance member when using decorator
+}
+
 function logError(errorMessage: string) {
-  // *** return
   return function (target: any, key: string, desc: PropertyDescriptor): void {
     const method = desc.value
 
@@ -26,10 +34,8 @@ function logError(errorMessage: string) {
       try {
         method()
       } catch (error) {
-        console.log(errorMessage) // ***
+        console.log(errorMessage)
       }
     }
   }
 }
-
-new Boat().pilot()
