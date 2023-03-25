52e6defb76ccd5b672ee347b67b4fb5b8d78b58e
hungbui7690 <hungbui7690@gmail.com>
2023-03-25 10:04:10 +0700

    The Refactoring Process & Prototypes Reminder

--- server/build/index.js
+++ server/build/index.js
@@ -1,18 +1,16 @@
 "use strict";
 /*
-  A Closer Integration
-  - pic
-  - previous lesson, we finished the 1st version, which is using the express library normally as we could with the bare minimum of TS
-    > minimum Type & Error checking in our application
-  - from now on, we will try to twist our lib to work with TS classes
-    > classes & interfaces
-  > below is an implementation (pic 1: left side)
-    > with the code below, it does not gain anything to our app:
-      + not easy to read
-      + not easy to write
-      > no benefit
+  The Refactoring Process
+  - we will use the ts-express-decorators lib (deprecated), and implement some of the features in that lib to our app
+    > pic
+  
+  > npm i ts-express-decorators
 
-  (***) this won't work > we will work on the right side of pic 1
+////////////////////////////////////////
+
+  Prototypes Reminder
+  - pic
+
 */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -22,17 +20,10 @@ const express_1 = __importDefault(require("express"));
 const loginRoutes_1 = require("./routes/loginRoutes");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-class Server {
-    constructor() {
-        this.app = (0, express_1.default)();
-        this.app.use(bodyParser.urlencoded({ extended: true }));
-        this.app.use(cookieSession({ keys: ['secret'] }));
-        this.app.use(loginRoutes_1.router);
-    }
-    start() {
-        this.app.listen(5000, () => {
-            console.log(`Listening on port 5000...`);
-        });
-    }
-}
-new Server().start();
+const app = (0, express_1.default)();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['secret'] }));
+app.use(loginRoutes_1.router);
+app.listen(5000, () => {
+    console.log(`Listening on port 5000...`);
+});
